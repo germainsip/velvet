d7409f17cd53c513e54f30ef65f96f22f333b168
Germain SIPIERE <gsipiere@gmail.com>
2020-05-03 16:55:55 +0200

    draggable

--- src/main/java/org/germain/App.java
+++ src/main/java/org/germain/App.java
@@ -2,9 +2,13 @@ package org.germain;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -13,13 +17,32 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
+    private double xOffset;
+    private double yOffset;
     @Override
     public void start(Stage primaryStage) throws IOException {
-Parent root = FXMLLoader.load(getClass().getResource("/org/germain/gui/catalogue.fxml"));
-Scene scene = new Scene(root);
+        Parent root = FXMLLoader.load(getClass().getResource("/org/germain/gui/catalogue.fxml"));
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.show();
 
-primaryStage.setScene(scene);
-primaryStage.show();
+
+        Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+        // on change les coordonnées de la fenêtre
+        primaryStage.setX(visualBounds.getMaxX() - 25 - scene.getWidth());
+        primaryStage.setY(visualBounds.getMinY() + 25);
+
+        //On fait glisser la fenêtre avec la souris
+        scene.setOnMousePressed(event -> {
+            xOffset = primaryStage.getX() - event.getScreenX();
+            yOffset = primaryStage.getY() - event.getScreenY();
+        });
+
+        scene.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() + xOffset);
+            primaryStage.setY(event.getScreenY() + yOffset);
+        });
     }
 }
--- src/main/java/org/germain/gui/CatalogueController.java
+++ src/main/java/org/germain/gui/CatalogueController.java
@@ -1,6 +1,8 @@
 package org.germain.gui;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import org.germain.App;
@@ -9,12 +11,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CatalogueController implements Initializable {
+    @FXML
     public ImageView displayAlb;
+    @FXML
+    public Label disc_title,disc_artist;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Image image = new Image(String.valueOf(getClass().getResource("pictures/Drama.jpeg")));
+        Image image = new Image(String.valueOf(getClass().getResource("pictures/Dirt.jpeg")));
         displayAlb.setImage(image);
-        System.out.println(getClass().getResource("Dirt.jpeg"));
+        disc_title.setText("Dirt");
+        disc_artist.setText("Alice In Chains");
+        System.out.println(getClass().getResource("pictures/Dirt.jpeg"));
     }
 }
