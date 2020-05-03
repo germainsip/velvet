892e0366b4162c88215d2ff657d22f5e9abfffe5
Germain SIPIERE <gsipiere@gmail.com>
2020-05-03 18:41:45 +0200

    style encours de recherche

--- src/main/java/org/germain/gui/CatalogueController.java
+++ src/main/java/org/germain/gui/CatalogueController.java
@@ -5,7 +5,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import org.germain.App;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,10 +15,12 @@ public class CatalogueController implements Initializable {
     public ImageView displayAlb;
     @FXML
     public Label disc_title,disc_artist;
+    public AnchorPane rootPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Image image = new Image(String.valueOf(getClass().getResource("pictures/Dirt.jpeg")));
+        //Image image = new Image(String.valueOf(getClass().getResource("pictures/Dirt.jpeg")));
+        Image image = new Image("http://localhost/velvet/assets/pictures/Highway%20To%20Hell.jpeg");
         displayAlb.setImage(image);
         disc_title.setText("Dirt");
         disc_artist.setText("Alice In Chains");
